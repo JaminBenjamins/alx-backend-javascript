f4ec2576945159b49379ee16d11ab60609d54ad4
JaminBenjamins <jaminbenjamins@gmail.com>
2023-09-28 01:04:12 +0300

    Returning a promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,7 @@
+// A function that returns a value and handles exception
+export default function getFullResponseFromAPI (success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(Error('The fake API is not working currently'));
+  });
+}
