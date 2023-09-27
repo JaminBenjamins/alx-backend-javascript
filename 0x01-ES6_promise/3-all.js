0f9c171fe708c911361badfe682f7a076b8837b5
JaminBenjamins <jaminbenjamins@gmail.com>
2023-09-27 23:45:58 +0300

    Setting the appropriate to handle imported variables

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
     const photo = uploadPhoto();
     const user = createUser();
-    return Promise.all(photo, user).then((result) => {
+    return Promise.all([photo, user]).then((result) => {
         console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
         });
         .catch(() => {
