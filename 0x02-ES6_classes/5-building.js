0258ca35efb07fbfe0f51d81a808cd86fc2feb03
JaminBenjamins <jaminbenjamins@gmail.com>
2023-09-29 19:38:35 +0300

    Defining a class with a function for handling error for inheritance

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+class Building {
+  constructor (sqft) {
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
+
+  get sqft () {
+    return this._sqft;
+  }
+
+  set sqft (value) {
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage () {
+    if (this.constructor.name !== 'Building') throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
